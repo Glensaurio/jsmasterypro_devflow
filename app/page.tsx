c9f2a39045fa7148eeb3ee9e77132deb18d836d6
Glensaurio <glenvw25@gmail.com>
2024-12-12 18:01:14 -0300

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-const Home = () => {
-  return <h1 className="h1-bold">Welcoome to the world of Next.js</h1>;
-};
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcoome to the world of Next.js</h1>;
+    <h1 className="h1-bold font-space-grotesk">
+      Welcoome to the world of Next.js
+    </h1>
+  </>
+);
 
 export default Home;
